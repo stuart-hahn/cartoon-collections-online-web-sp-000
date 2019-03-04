28ba72d0f1f6d863f31782c327c4dba7df7ecda1
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-04 21:53:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(elements)# code an argument here
   elements.map {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
