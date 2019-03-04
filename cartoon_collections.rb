4f02e1492f5a9cae62b218150c001c91f87cf86a
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-04 21:44:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+  elements.each! {|element| element.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
